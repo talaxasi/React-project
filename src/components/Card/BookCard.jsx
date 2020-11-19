dce55df716431a37c418c4a96b85e481b3b2fa21
talaxasi <kiriesk2@mail.ru>
2020-11-19 19:52:35 +0300

    fix mistakes - day 2)

--- src/components/Card/BookCard.jsx
+++ src/components/Card/BookCard.jsx
@@ -1,11 +1,48 @@
 import React from "react";
 import {connect} from "react-redux";
-import PureBookCard from "./PureBookCard";
 import {setFavoriteBook, deleteFavoriteBook} from "../../store/favorites/favoritesActions";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import {Link} from "react-router-dom";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import ButtonFavorites from "../ButtonFavorites/ButtonFavorites";
+import './BookCard.scss';
 
 const BookCard = (props) => {
+  const {image, title, author, price, rating, id} = props.bookCard;
+
   return (
-    <PureBookCard props={props}/>
+    <Card className={"bookCard"}>
+      <CardActionArea>
+        <Link to={`/${id}`}>
+          <CardMedia
+            className={"cardMedia"}
+            image={image}
+            title={title}
+          />
+        </Link>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {author}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Typography>
+          Цена: {price}
+        </Typography>
+        <Typography>
+          Рейтинг: {rating}
+        </Typography>
+      </CardActions>
+      <ButtonFavorites props={props}/>
+    </Card>
   );
 }
 
--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {connect} from "react-redux";
-import PureFavorites from "./PureFavorites";
 import {setFavoriteBook, deleteFavoriteBook} from "../../store/favorites/favoritesActions";
+import {Link} from "react-router-dom";
+import './Favorites.scss';
+
+const loadLocalStoreFavorites = key => {
+  try {
+    const favorites = localStorage.getItem(key);
+    const localStorageFavorites = JSON.parse(favorites);
+    return localStorageFavorites;
+  } catch (e) {
+    console.log('loadLocalStoreFavorites', e)
+  }
+}
 
 const Favorites = (props) => {
+
+  const [favorites, setFavorites] = useState(loadLocalStoreFavorites('favorites'));
+  const [books, setBooks] = useState({});
+
+  useEffect(() => {
+    fetch('http://localhost:3001/books')
+      .then(response => response.json())
+      .then(data => setBooks(data))
+  }, []);
+
+  if (!favorites) return "Loading... state";
+  if (books[0] == undefined) return "Loading... books";
   return (
-    <PureFavorites props={props}/>
+    <div className={"PureFavorites"}>
+      <h1>Избранное!</h1>
+
+      <Link to={'/'}>
+        <button>Назад</button>
+      </Link>
+      <div className={"favoriteBooks"}>
+        {favorites.map((id) => (
+          <div key={books[id].title} className={"favoriteBooks-item"}>
+            <h3 className={"favoriteBooks-item__h3"}>{books[id].title}</h3>
+            <div className={"image"}>
+              <img src={books[id].image} alt={books[id].title}/>
+            </div>
+            <Link to={`/${id}`}>
+              <p className={"favoriteBooks-item__p"}>к карточке товара</p>
+            </Link>
+            <button onClick={() => {
+              props.deleteFavoriteBook(books[id]);
+              setFavorites(loadLocalStoreFavorites('favorites'))
+            }}>&times;</button>
+          </div>))}
+      </div>
+    </div>
   );
 }
 
